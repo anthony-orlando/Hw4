ecd066e01b7747026c29af1bafb49931c6aa77fd
Anthony Orlando <anthony-orlando@uiowa.edu>
2015-02-23 14:34:09 -0600

    add vb code to the web page

--- Default.aspx
+++ Default.aspx
@@ -24,13 +24,23 @@
         <br />
         <asp:Label ID="lb_preTax" runat="server" Text="Pre-tax Deductions:"></asp:Label>
         <asp:TextBox ID="tb_preTax" runat="server"></asp:TextBox>
-        <br />
+        &nbsp;<br />
         <br />
         <asp:Label ID="lb_afterTax" runat="server" Text="After tax deductions:"></asp:Label>
         <asp:TextBox ID="tb_afterTax" runat="server"></asp:TextBox>
         <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <br />
+&nbsp;<asp:Button ID="bt_calculate" runat="server" Text="Calculate" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="bt_clear" runat="server" Text="Clear" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />
+        <br />
+&nbsp;
         <asp:Label ID="lb_results" runat="server"></asp:Label>
+    
+        <br />
     
     </div>
     </form>
--- Default.aspx.vb
+++ Default.aspx.vb
@@ -2,4 +2,34 @@
 Partial Class _Default
     Inherits System.Web.UI.Page
 
+    Protected Sub bt_calculate_Click(sender As Object, e As EventArgs) Handles bt_calculate.Click
+        Dim hours As Decimal = Val(tb_hoursWorked.Text)
+        Dim wage As Decimal = Val(tb_hourlyWage.Text)
+        Dim preTax As Decimal = Val(tb_preTax.Text)
+        Dim afterTax As Decimal = Val(tb_afterTax.Text)
+        Dim taxRate As Decimal = 0
+        Dim netPay As Decimal = 0
+
+        If (hours * wage) - preTax < 500 Then
+            taxRate = 0.18
+            netPay = (((hours * wage) - preTax) * (1 - taxRate)) - afterTax
+        Else
+            taxRate = 0.22
+            netPay = (((hours * wage) - preTax) * (1 - taxRate)) - afterTax
+
+        End If
+
+        lb_results.Text = "Your net pay for this week is, " & String.Format("{0:C}", netPay)
+        bt_calculate.Enabled = False
+    End Sub
+
+    Protected Sub bt_clear_Click(sender As Object, e As EventArgs) Handles bt_clear.Click
+        tb_hoursWorked.Text = String.Empty
+        tb_hourlyWage.Text = String.Empty
+        tb_preTax.Text = String.Empty
+        tb_afterTax.Text = String.Empty
+        lb_results.Text = String.Empty
+
+        bt_calculate.Enabled = True
+    End Sub
 End Class
